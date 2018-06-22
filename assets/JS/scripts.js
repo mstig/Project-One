b8440dee4a42625110a73bfa46b338b607fa2a82
timkomo <timko.matthew@gmail.com>
2018-06-21 20:49:41 -0400

    worked on api to display data on page.

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -14,43 +14,83 @@ $(document).ready(function () {
         $("#startMenu").hide(1000);
 
     })
-    
-    $("#submitBandBtn").on("click", function(event) {
-        
 
-        
+    $("#submitBandBtn").on("click", function () {
+        if ($("#band-input").val().trim() === "") {
+            alert("you must enter a band.")
+        }
+
+
+
 
         var band = $("#band-input").val().trim()
-        var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + band + "&api_key=43aa7275eb736bbda8af4906bb03dfaa&format=json"
+        var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + band + "&api_key=43aa7275eb736bbda8af4906bb03dfaa&format=json"
 
-        
+
         // Creating an AJAX call for the specific band button being clicked
         $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          
-          var bandsReturn = response.similarartists.artist
-          console.log(bandsReturn);
-  
-          for (i = 0; i < 10; i++) { 
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var bandsReturn = response.similarartists.artist
+
+            $("#bands-view").append("<h1> Similar Artists to " + band);
+            $("#bands-view").append("<h3> Click a band to learn more!");
+
+            for (i = 0; i < 5; i++) {
                 console.log(bandsReturn[i].name);
 
-            $("#enterBand-Div").hide(1000);
-            $("#bands-view-header").text("Similar Artists to " + band);
-            $("#bands-view-header").show();
-            $("#bands-view").show(1000);
-            $("#bands-view").append("<li hidden>" + bandsReturn[i].name + "</li>" );
-            $("li").show(2000);
-               
-          }
+                $("#enterBand-Div").hide(1000);
+                $("#bands-view").show(1000);
 
-       
-   
-   
+                bandButtons = $("<button hidden>" + bandsReturn[i].name + "</button>");
+                $("#bands-view").append(bandButtons);
+                $(bandButtons).addClass("band-return")
+                $(bandButtons).attr("data-band", bandsReturn[i].name)
+                $(".band-return").show(2000);
+
+            }
+
+
+
+
+
+
+        })
+
+        $(document).on("click", ".band-return", function (event) {
+
+            var bandInfo = $(this).attr("data-band");
+            var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + bandInfo + "&api_key=43aa7275eb736bbda8af4906bb03dfaa&format=json";
+
+
+            console.log(bandInfo)
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+
+                console.log(response);
+
+                var bandPic = response.artist.image[3]["#text"];
+                var bandBio = response.artist.bio.summary;
+
+                console.log(bandPic);
+                console.log(bandBio);
+                
+                $("#bands-view").append("<h1>" + bandInfo + "</h1>")
+                $("<img>").attr("src", bandPic).appendTo("#bands-view");
+                $("#bands-view").append("<p>" + bandBio + "</p>");
+
+
+
+            })
+
+        })
+
     })
-
-})
 
 
 
