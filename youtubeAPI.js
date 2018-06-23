c07dd8e597667322363bbb4070f6adbb61e7704f
Mason Stigall <masonstigall@gmail.com>
2018-06-23 13:22:45 -0400

    tweaking api display

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -1,13 +1,12 @@
 
 var searchTerm = "the black keys";
 var googleApiKey = "AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM";
-var youtubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&relevanceLanguage=en&regionCode=US&type=video&q=" + searchTerm + "&key=AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM";
-var playerId = "D9ioyEvdggk";
+var youtubeURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&relevanceLanguage=en&regionCode=US&q=' + searchTerm + '&key=AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM&type=video';
 $(document).ready(function() {
     $.ajax({ url: youtubeURL, method: "GET" }).then(function (response) {
         console.log(response);
-        console.log(response.items[0].id.videoId);
-        playerId = response.items[0].id.videoId;
+        console.log(response.items[1].id.videoId);
+        var playerId = response.items[1].id.videoId;
         $("#player").append('<iframe width="500" height="315" src="http://www.youtube.com/embed/'+playerId+'" frameborder="0" allowfullscreen></iframe> ' );
        
     });
