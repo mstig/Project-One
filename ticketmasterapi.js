5119f14d72ff6e20da14f86f7335c0557d4c96c1
Mason Stigall <masonstigall@gmail.com>
2018-06-26 20:39:51 -0400

    including google api conversion under ticketmasterindex.html file

--- ticketmasterapi.js
+++ ticketmasterapi.js
@@ -1,9 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function () {  
 
-    var bandName = "";
-    var ticketAPI = "ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M";
-    var zipCode = 23113; //user will input this and grab from page
-    var searchKey = "";
+    ///////////////////////////////////////////////////////////////
+    // zip + google api conversion //////////////////////////////
+
+    // Need to add a zip input section on the first page\
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
     var zipCode = 23113; //user will input this and grab from page
     var zipURL = "http://maps.googleapis.com/maps/api/geocode/json?address=";
@@ -16,8 +17,14 @@ $(document).ready(function () {
         latLong = (response.results[0].geometry.location.lat + ","+ response.results[0].geometry.location.lng);
      });
 
-     //plug latLong variable into end of ticketmaster widget plugin under w-latlong
+     //example zip check:
+    if (1 < zipCode <= 99999) {
+        console.log("zip test");
+    }
 
+    // End google zip block
+
+    //Lat Long will be plugged into w-latlong at the very end of the ticketmaster widget append
     $("#submit-button").on("click", function (event) {
         event.preventDefault();
         bandName = $("#search-item").val().trim();
