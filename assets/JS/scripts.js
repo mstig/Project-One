b2b6d545055d2ba6db6040094aad71011474dbe6
Patrick Lehmann <patlehmann1@gmail.com>
2018-06-28 19:15:29 -0400

    removed console logs

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -11,12 +11,10 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
     /// start up inputs on load
-
     function ticketmasterShow(bandGenre, latLong) {
         $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="dark" w-width="375" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="' + latLong + '"></div>');
         $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
     }
-
     $("#startMenu,#startBtn").fadeIn(2500).removeClass('hidden');
     $("#startBtn").click(function () {
         $("#startBtn").hide();
@@ -46,19 +44,15 @@ $(document).ready(function () {
             // Creating an AJAX call for the specific band button being clicked
             $.ajax({ url: zipURL + zipCode, method: "GET" }).then(function (results) {
                 latLong = results.results[0].geometry.location.lat + "," + results.results[0].geometry.location.lng;
-
-
                 $.ajax({
                     url: queryURL,
                     method: "GET"
                 }).then(function (response) {
                     // console.log(response);
-
                     var bandsReturn = response.artist.similar.artist;
                     var bandGenre = response.artist.tags.tag[0].name;
                     // console.log(bandGenre);
                     // console.log(bandsReturn);
-
                     if (bandsReturn.length === 0) {
                         $("#bands-view").show(1000);
                         $("#bands-view").append("<h3> No similar artists for  " + band);
@@ -74,7 +68,6 @@ $(document).ready(function () {
                     $("#bands-view").append("<h3> SIMILAR ARTISTS TO " + band);
                     $("#bands-view").append("<h5> Click a band to learn more!");
                     for (i = 0; i < 5; i++) {
-                        console.log(bandsReturn[i].name);
                         $("#enterBand-Div").hide(1000);
                         $("#bands-view").show(1000);
                         bandButtons = $("<button hidden>" + bandsReturn[i].name + "</button>");
@@ -89,9 +82,6 @@ $(document).ready(function () {
             })
         }
     })
-
-
-
     $(document).on("click", ".band-return", function (event) {
         event.preventDefault();
         var bandInfo = $(this).attr("data-band");
@@ -100,11 +90,8 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
             var bandPic = response.artist.image[3]["#text"];
             var bandBio = response.artist.bio.summary;
-            console.log(bandPic);
-            console.log(bandBio);
             $("#band-info-div").empty();
             $("#band-info-div").append("<h1 class='scroll-head'>" + bandInfo + "</h1>")
             $("<img class='img-thumbnail'>").attr("src", bandPic).appendTo("#band-info-div");
@@ -113,8 +100,6 @@ $(document).ready(function () {
             $("html, body").animate({ scrollTop: $(".scroll-head").offset().top }, 'slow');
             var youtubeURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&relevanceLanguage=en&regionCode=US&q=' + bandInfo + '&key=AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM&type=video';
             $.ajax({ url: youtubeURL, method: "GET" }).then(function (response) {
-                console.log(response);
-                console.log(response.items[1].id.videoId);
                 var playerId = response.items[1].id.videoId;
                 $("#band-info-div").append("<p>Listen to a sample of " + bandInfo + ":");
                 $("#band-info-div").append('<iframe width="500" height="315" src="https://www.youtube.com/embed/' + playerId + '" frameborder="0" allowfullscreen></iframe> ');
@@ -126,10 +111,8 @@ $(document).ready(function () {
         $('#modal2').modal("open");
     });
     database.ref().orderByChild("dateAdded").limitToLast(10).on("value", function (childSnapshot) {
-        console.log(childSnapshot.val());
         $("#recent-searches").empty();
         for (let val of Object.values(childSnapshot.val())) {
-            console.log(val.band);
             recentButtons = $("<button>" + val.band + "</button>");
             $(recentButtons).addClass("waves-effect waves-light btn-large");
             $("#recent-searches").append(recentButtons);
@@ -139,7 +122,6 @@ $(document).ready(function () {
     });
     $(document).on("click", ".recent-band-return", function (event) {
         var inputRecentBand = $(this).attr("data-band");
-        console.log(inputRecentBand);
         $("#band-input-label").addClass("active");
         $("#band-input").val(inputRecentBand);
     })
@@ -150,10 +132,8 @@ $(document).ready(function () {
     });
 
     database.ref().orderByChild("dateAdded").limitToLast(10).on("value", function (childSnapshot) {
-        console.log(childSnapshot.val());
         $("#recent-searches").empty();
         for (let val of Object.values(childSnapshot.val())) {
-            console.log(val.band);
             recentButtons = $("<button>" + val.band + "</button>");
             $(recentButtons).attr("id", "band-button2");
             $(recentButtons).addClass("waves-effect waves-light btn-large");
@@ -164,7 +144,6 @@ $(document).ready(function () {
     });
     $(document).on("click", ".recent-band-return", function (event) {
         var inputRecentBand = $(this).attr("data-band");
-        console.log(inputRecentBand);
         $("#band-input-label").addClass("active");
         $("#band-input").val(inputRecentBand);
     })
