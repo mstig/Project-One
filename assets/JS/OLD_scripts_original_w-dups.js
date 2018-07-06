e8e02ca5799f2a627e40b1be1ed9ecbb63668781
Syd Vicious <cjschwichtenberg@gmail.com>
2018-07-06 03:56:04 -0400

    Fixed scrolling issue on similar-artist div when populated from recent-user-search buttons in Modal2. Added modal property and destoyed modal function upon recent-search button on click event. Added functionality comments for easier collaborative editing in the future. Cleaned up code, deleted two blocks of duplicated code blocks unneeded for app functionality.

--- assets/JS/OLD_scripts_original_w-dups.js
+++ assets/JS/OLD_scripts_original_w-dups.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    
-
     // Initializing Firebase
     var config = {
         apiKey: "AIzaSyDnUGtk_r_yXM12lOFbZNZp6iZETkMtPNE",
@@ -13,13 +11,11 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
     
-
     /// start up inputs on load
     function ticketmasterShow(bandGenre, latLong) {
         $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="dark" w-width="375" w-height="425" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="' + latLong + '"></div>');
         $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
     }
-
 
     // Start Button click event- triggers JS animations for band-name/zip input
     $("#startMenu,#startBtn").fadeIn(2500).removeClass('hidden');
@@ -30,49 +26,38 @@ $(document).ready(function () {
         $("#startMenu").hide(1000);
     })
 
-
     // Submit Band button click event
     $("#submitBandBtn").on("click", function () {
-        
         // prevents page from reloading
         event.preventDefault();
-        
-        // Input Validation for band-name & zip-code- If band-name/zip-code missing- initialize & open Modal1
+        // Input Validation for band-name & zip-code form sections
         if ($("#band-input").val().trim() === "" || $("#zip-input").val().trim() === "") {
+            // If band-name or zip-code missing- initialize and open Modal1
             $('#modal1').modal();
             $('#modal1').modal("open");
-        // Modal1 triggered - stop function
+            // When Modal1 triggered- stop on click event function from continuing
             return false;
-        
-        // Modal1 not triggered - continuing on click event function
+        // If Modal1 not triggered- continuing on click event function
         } else {
-        
             // JS animations to display similar bands & TicketMaster divs
             $("#jumbotron1").hide(1000);
             $("#jumbotron2, #jumbotron3").show(2000);
-        
-            // trimming band-name & zip-code whitespace for use in API calls
+            // trimming band-name & zip-code whitespace for APi calls
             var band = $("#band-input").val().trim().toUpperCase();
             var zipCode = $("#zip-input").val().trim();
-            
-            // Google API for zip-code- changes zip top lat/long so TicketMaster search provides local results by zip
+            // Google API for zipcode- changes zip top lat/long so TicketMaster search provides local results by zip
             var zipURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
             var latLong;
-            
             // Last.FM API Call
             var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + band + "&api_key=43aa7275eb736bbda8af4906bb03dfaa&format=json"
-            
             // Appending TicketMaster Div with widget 
             $("#ticketmaster-view").prepend("<h5> SHOWS NEAR YOU SIMILAR TO " + band);
-            
-            // Pushes user's band-name search to Firebase for storage- ordered by Timestamp input in Firebase 
+            // Pushing the user's band-name search to Firebase for use to display as recent search for new user- ordered by Timestamp input within FireBase
             database.ref().push({
                 band: band,
                 dateAdded: firebase.database.ServerValue.TIMESTAMP,
             });
-
-            // Creating AJAX call for Google API - zip-code input changed to lat/long for correct TicketMaster API parameters needed for local show results
-
+            // Creating an AJAX call for the specific band button being clicked to display local search results
             $.ajax({ url: zipURL + zipCode, method: "GET" }).then(function (results) {
                 latLong = results.results[0].geometry.location.lat + "," + results.results[0].geometry.location.lng;
 
@@ -80,32 +65,32 @@ $(document).ready(function () {
                 $.ajax({
                     url: queryURL,
                     method: "GET"
-
-                // Last.FM Response Handler- storing similar artist/genre from response in variable
+                // Last.FM API Response to be consoled & return similar artist
                 }).then(function (response) {
+                    // console.log(response);
 
                     var bandsReturn = response.artist.similar.artist;
                     var bandGenre = response.artist.tags.tag[0].name;
+                    // console.log(bandGenre);
+                    // console.log(bandsReturn);
 
-                    // Input Validation - if Last.FM CANNOT return results for similar artist
-                    // Displays message to user that search could not produce similar artist results ***still allows user to view TicketMaster widget for local shows***
+                    // Input Validation if Last.FM does not return any results for band-name searched similar artist
+                    // Displays to user that band-name search did not produce similar artist results, but still allows user to view TM widget for local shows
                     if (bandsReturn.length === 0) {
                         $("#bands-view").show(1000);
                         $("#bands-view").append("<h3> No similar artists for  " + band);
                         $("#bands-view").append("<h5> Try searching for another band! ");
                         ticketmasterShow(bandGenre, latLong);
-
-            //*** MASON *** Unsure why commented out ????? - please check & delete if no longer needed
-                    // $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="light" w-width="300" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="37.5462865,-77.4089139"></div>');
-                    // $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
+                        // $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="light" w-width="300" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="37.5462865,-77.4089139"></div>');
+                        // $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
                         return false;
                     }
+                    // TicketMaster call to display local results by zipcode
+                    // User zip-code input first changed to lat/long for correct TicketMaster API parameters needed
                     ticketmasterShow(bandGenre, latLong);
-            // *** MASON *** Unsure why commented out ????? - please check & delete if no longer needed
-                    // $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="light" w-width="300" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="37.5462865,-77.4089139"></div>');
-                    // $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
-
-                    // Creates similar-artist buttons with JS animation- loops to create 5 buttons from Last.FM response
+                            // $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="light" w-width="300" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="37.5462865,-77.4089139"></div>');
+                            // $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
+                    // Creates similar-artist buttons with JS animation- loops through create 5 buttons from results
                     $("#bands-view").append("<h3> SIMILAR ARTISTS TO " + band);
                     $("#bands-view").append("<h5> Click a band to learn more!");
                     for (i = 0; i < 5; i++) {
@@ -126,77 +111,87 @@ $(document).ready(function () {
         }
     })
 
-
-    // On click event- handles displaying band info from Last.FM (image, summary & link)
+    // On click event to handle displaying info from Last.FM API about similar-artists(-displays band name, image, summary & link to more info on Last.FM website-)
     $(document).on("click", ".band-return", function (event) {
-        
         // Prevents page from reloading
         event.preventDefault();
-        
-        // New last.FM AJAX Call to retrieve band info
+        // $('#modal2').modal("close");
+        // New last.FM AJAX Call to retrieve individual band summary & image
         var bandInfo = $(this).attr("data-band");
         var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + bandInfo + "&api_key=43aa7275eb736bbda8af4906bb03dfaa&format=json";
         $.ajax({
             url: queryURL,
             method: "GET"
-        
-        // Response handler from Last.FM
+        // Response from Last.FM
         }).then(function (response) {
             console.log(response);
             var bandPic = response.artist.image[3]["#text"];
             var bandBio = response.artist.bio.summary;
             console.log(bandPic);
             console.log(bandBio);
-
-            // Displaying band info/summary/image
+            // Displaying band summary and image
             $("#band-info-div").empty();
             $("#band-info-div").append("<h1>" + bandInfo + "</h1>")
             $("<img class='img-thumbnail'>").attr("src", bandPic).appendTo("#band-info-div");
             $("#band-info-div").append("<br>");
             $("#band-info-div").append("<p>" + bandBio + "</p>");
-           
-            // YouTube API - AJAX Call & Response Handler to display band video sample
+            // $("html, body").animate({ scrollTop: $(".scroll-head").offset().top }, 'slow');
+            // YoutTube API AJAX Call/Response and response for band video sample
             var youtubeURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&relevanceLanguage=en&regionCode=US&q=' + bandInfo + '&key=AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM&type=video';
             $.ajax({ url: youtubeURL, method: "GET" }).then(function (response) {
                 console.log(response);
                 console.log(response.items[1].id.videoId);
                 var playerId = response.items[1].id.videoId;
-                
-                // Displays YouTube video
+                // Displaying YouTube video
                 $("#band-info-div").append("<p>Listen to a sample of " + bandInfo + ":");
                 $("#band-info-div").append('<iframe width="500" height="315" src="https://www.youtube.com/embed/' + playerId + '" frameborder="0" allowfullscreen></iframe> ');
             })
         })
     })
 
+// DUPLICATE CODE- See line 182-185
+    // // Opening recent searches modal upon on click event button
+    //     $(document).on("click", "#recent_searches", function (event) {
+    //     $('#modal2').modal();
+    //     $('#modal2').modal("open");
+    // });
 
-    // On click event - adds class and changed val to band-name button clicked by user - Needed for Last.FM API call
+// DUPLICATE CODE- See line 188-200- Only difference is below adds an ID for CSS Style purposed
+    // database.ref().orderByChild("dateAdded").limitToLast(10).on("value", function (childSnapshot) {
+    //     console.log(childSnapshot.val());
+    //     $("#recent-searches").empty();
+    //     for (let val of Object.values(childSnapshot.val())) {
+    //         console.log(val.band);
+    //         recentButtons = $("<button>" + val.band + "</button>");
+    //         $(recentButtons).addClass("waves-effect waves-light btn-large");
+    //         $("#recent-searches").append(recentButtons);
+    //         $(recentButtons).addClass("recent-band-return")
+    //         $(recentButtons).attr("data-band", val.band);
+    //     };
+    // });
+
+    // On click event that add class and changed val to bnand-name button clicked by user for use in Last.FM API call
     $(document).on("click", ".recent-band-return", function (event) {
         var inputRecentBand = $(this).attr("data-band");
         console.log(inputRecentBand);
         $("#band-input-label").addClass("active");
         $("#band-input").val(inputRecentBand);
-    });
+    })
 
-
-    // Initialize & opens Modal2 to display recent searches from firebase upon on click event from form button
+    // Opening recent searches modal upon on click event button
     $(document).on("click", "#recent_searches", function (event) {
-        
-        // *** Bug Fix *** Sets modal options to allow for scrolling when open AND closed -
-        $('#modal2').modal({
-            preventScrolling: false
-        });
+        $('#modal2').modal();
         $('#modal2').modal("open");
     });
 
-
-    // Loop to displays & limit recent-search buttons to last 10 user searches - displays buttons to Modal2
+    // Limiting recent search buttons to last 10 user searches and displaying them to Modal at the bottom of page
     database.ref().orderByChild("dateAdded").limitToLast(10).on("value", function (childSnapshot) {
         console.log(childSnapshot.val());
         $("#recent-searches").empty();
         for (let val of Object.values(childSnapshot.val())) {
             console.log(val.band);
             recentButtons = $("<button>" + val.band + "</button>");
+            // Below: only line not duplicated above in lines160-171 ** adds ID attr for CSS style -NEEDED **
             $(recentButtons).attr("id", "band-button2");
             $(recentButtons).addClass("waves-effect waves-light btn-large");
             $("#recent-searches").append(recentButtons);
@@ -205,19 +200,18 @@ $(document).ready(function () {
         };
     });
 
-
-    // On click event handler for recent-search buttons 
+    // If user clicks on recent-search buttons- displays band-name input and changs value of band-name input section above in the HTML 
+    // Displays recent searches name vs. user manually inputting band-name
     $(document).on("click", ".recent-band-return", function (event) {
         var inputRecentBand = $(this).attr("data-band");
         console.log(inputRecentBand);
-        // Adds active class, changes value equal to band-name & displays band-name from recent-serarches in band-name input on form for user        
         $("#band-input-label").addClass("active");
         $("#band-input").val(inputRecentBand);
         $('#modal2').modal("destroy");
     })
 
-
-    // Sticky Scrolling function - allows TM widget to remain visiable/centered while scrolling through similar-band info div
+    // Sticky Scrolling function on similar-artist
+    // Makes TM widget always visiable/centered while user scrolls similar band summary/image/video displayed as an inline div to widget on page
     $(window).scroll(function () {
         $("#jumbotron3").css({
             "top": ($(window).scrollTop()) + "px",
@@ -225,7 +219,5 @@ $(document).ready(function () {
         });
     });
 
-
 });
-
 
