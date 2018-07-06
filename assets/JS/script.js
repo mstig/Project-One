b25e1ff9756d797e4dd35de6bd4734713fcf0109
Mason Stigall <masonstigall@gmail.com>
2018-07-06 11:39:44 -0400

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -95,16 +95,9 @@ $(document).ready(function () {
                         $("#bands-view").append("<h5> Try searching for another band! ");
                         ticketmasterShow(bandGenre, latLong);
 
-            //*** MASON *** Unsure why commented out ????? - please check & delete if no longer needed
-                    // $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="light" w-width="300" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="37.5462865,-77.4089139"></div>');
-                    // $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
-                        return false;
+                         return false;
                     }
-                    ticketmasterShow(bandGenre, latLong);
-            // *** MASON *** Unsure why commented out ????? - please check & delete if no longer needed
-                    // $("#widget").append('<div w-type="event-discovery" w-tmapikey="ITMP1WL5haYqT4ySnnZVTYi5HEV0QB3M" w-googleapikey="AIzaSyDLaes9_vXmELG_d5SGPPGNelBrWiHIkLM" w-keyword="' + bandGenre + '" w-theme="listviewthumbnails" w-colorscheme="light" w-width="300" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="100" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_msj025" w-source="" w-latlong="37.5462865,-77.4089139"></div>');
-                    // $("#widget").append('<script src="https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js"></script>');
-
+          
                     // Creates similar-artist buttons with JS animation- loops to create 5 buttons from Last.FM response
                     $("#bands-view").append("<h3> SIMILAR ARTISTS TO " + band);
                     $("#bands-view").append("<h5> Click a band to learn more!");
